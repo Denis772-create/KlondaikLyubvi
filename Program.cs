3f511a63bedb4b77cd2853f4fd275e21d99f90d3
d.aleksandrov <d.aleksandrov@cargonomica.com>
2025-08-22 18:32:43 +0300

    фв

--- Program.cs
+++ Program.cs
@@ -29,7 +29,6 @@ builder.Services.AddScoped<HttpClient>(sp =>
 
 var app = builder.Build();
 
-// Apply EF Core migrations at startup
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
